3aff3c97c3a5e210b9f22daee71acefef8323120
Andreas Trøstrup <8415722+Duckth@users.noreply.github.com>
2023-11-09 20:25:27 +0100

    Convert ProductDto to ProductResponse

--- Shifty.App/Repositories/IProductRepository.cs
+++ Shifty.App/Repositories/IProductRepository.cs
@@ -2,11 +2,12 @@ using System.Threading.Tasks;
 using LanguageExt;
 using LanguageExt.Common;
 using Shifty.Api.Generated.AnalogCoreV1;
+using Shifty.Api.Generated.AnalogCoreV2;
 
 namespace Shifty.App.Repositories
 {
     public interface IProductRepository
     {
-        public Task<Try<System.Collections.Generic.IEnumerable<ProductDto>>> GetProducts();
+        public Task<Try<System.Collections.Generic.IEnumerable<ProductResponse>>> GetProducts();
     }
 }
