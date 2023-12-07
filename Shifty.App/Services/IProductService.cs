97e2a773339de268e27ee38f2afcafaeac41218f
A-Guldborg <aguh@itu.dk>
2023-12-07 20:28:31 +0100

    Documentation of the ProductService

--- Shifty.App/Services/IProductService.cs
+++ Shifty.App/Services/IProductService.cs
@@ -14,7 +14,19 @@ namespace Shifty.App.Services
         /// </summary>
         /// <returns>Collection of products in the form of ProductDtos. The Collection is null if an error happens.</returns>
         Task<Try<IEnumerable<ProductResponse>>> GetProducts();
+
+        /// <summary>
+        /// Updates a product
+        /// </summary>
+        /// <param name="product">The product request containing the updated product information</param>
+        /// <returns>The updated product</returns>
         Task<Try<ChangedProductResponse>> UpdateProduct(UpdateProductRequest product);
+
+        /// <summary>
+        /// Adds a product
+        /// </summary>
+        /// <param name="product">The product request containing the new product information</param>
+        /// <returns>The newly created product</returns>
         Task<Try<ChangedProductResponse>> AddProduct(AddProductRequest product);
     }
 }
