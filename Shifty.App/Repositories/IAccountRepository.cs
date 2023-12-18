1e35a0b1fd5474fee4e42189fce221fb7f86c562
A-Guldborg <aguh@itu.dk>
2023-12-19 00:59:08 +0100

    Adding PoC user manager

--- Shifty.App/Repositories/IAccountRepository.cs
+++ Shifty.App/Repositories/IAccountRepository.cs
@@ -1,12 +1,16 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using LanguageExt;
 using LanguageExt.Common;
 using Shifty.Api.Generated.AnalogCoreV1;
+using Shifty.Api.Generated.AnalogCoreV2;
 
 namespace Shifty.App.Repositories
 {
     public interface IAccountRepository
     {
         public Task<Either<Error, TokenDto>> LoginAsync(string username, string password);
+        public Task<Try<ICollection<UserSearchResponse>>> SearchUserAsync(string query, int page, int pageSize);
+        public Task<Try<Task>> UpdateUserGroupAsync(int userId, UserGroup group);
     }
 }
---
+++ Shifty.App/Services/IUserService.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LanguageExt;
+using LanguageExt.Common;
+using Shifty.Api.Generated.AnalogCoreV2;
+
+namespace Shifty.App.Services
+{
+    public interface IUserService
+    {
+        Task<Try<ICollection<UserSearchResponse>>> SearchUsers(string query, int pageNumber = 0, int pageSize = 10);
+        Task<Try<Task>> UpdateUserGroupAsync(int userId, UserGroup group);
+    }
+}
